f0148f58baa9965b195517edae11776b714ff255
m2cci-garciacp <pablo.garcia-campos@etu.univ-grenoble-alpes.fr>
2023-11-10 15:53:07 +0100

    ajout sanitation nombrecache TP7

--- TP7/nombrecache/js/game.js
+++ TP7/nombrecache/js/game.js
@@ -1,60 +1,7 @@
 let tryLefts = 10;
-let hiddenNumber=Math.floor(Math.random() * 101);
+let hiddenNumber = Math.floor(Math.random() * 101);
 let newgame = false;
 
-
-document.getElementById("play").addEventListener("click",  () => {
-    let guess = document.getElementById("guess").value;
-    let msg1 = "";
-    let msg2 = "";
-    let mode;
-
-    tryLefts-=1;
-
-    if (guess > hiddenNumber) {
-        msg1 = "Trop grand!\n";
-        msg2 = "Rejouez, il vous reste "+ tryLefts+ " essais.";
-        mode = "hot";
-    } else if (guess < hiddenNumber) {
-        msg1 = "Trop petit!\n";
-        msg2 = "Rejouez, il vous reste "+ tryLefts+ " essais.";
-        mode = "cold";
-    } else {
-        msg1 = "Bravo!\nVous avez gagnez en "+ (10-tryLefts)+ " essais.";
-        mode = "success";
-        newgame = true;
-    }
-
-    if (tryLefts === 0 && !newgame ) {
-        msg2 = "Vous avez perdu, le numero était " + hiddenNumber;
-        mode = "failure";
-        newgame = true;
-    } 
-
-    // update message
-    print_msg(msg1+msg2, mode);
-
-    // update buttons
-    if (newgame) {
-        toNewGame();
-    } else {
-        inGame();
-    }
-})
-
-document.getElementById("forfeit").addEventListener("click", () => {
-    let msg = "Pourquoi vous abandonnez??\nVous avez perdu, le numero était " + hiddenNumber;
-    let mode = "failure";
-
-    // update message
-    print_msg(msg, mode);
-
-    // update buttons
-    toNewGame();
-    }
-)
-
-document.getElementById("newgame").addEventListener("click", () => {print_msg("", "normal");inGame();});
 
 function inGame() {
     document.getElementById("play").classList.remove("hidden");
@@ -78,3 +25,74 @@ function print_msg(msg, mode) {
     textBox.className = "";
     textBox.classList.add(mode);
 }
+
+function turn () {
+    let guess = document.getElementById("guess");
+    let msg1 = "";
+    let msg2 = "";
+    let mode;
+    if (!numberIsValid(guess.value, guess.min, guess.max)) {
+        print_msg("Entree erronee!", "failure");
+        return 0;
+    } else {
+        guess = parseInt(guess.value);
+    }
+
+    tryLefts -= 1;
+
+    if (guess > hiddenNumber) {
+        msg1 = "Trop grand!\n";
+        msg2 = "Rejouez, il vous reste " + tryLefts + " essais.";
+        mode = "hot";
+    } else if (guess < hiddenNumber) {
+        msg1 = "Trop petit!\n";
+        msg2 = "Rejouez, il vous reste " + tryLefts + " essais.";
+        mode = "cold";
+    } else {
+        msg1 = "Bravo!\nVous avez gagnez en " + (10 - tryLefts) + " essais.";
+        mode = "success";
+        newgame = true;
+    }
+
+    if (tryLefts === 0 && !newgame) {
+        msg2 = "Vous avez perdu, le numero était " + hiddenNumber;
+        mode = "failure";
+        newgame = true;
+    }
+
+    // update message
+    print_msg(msg1 + msg2, mode);
+
+    // update buttons
+    if (newgame) {
+        toNewGame();
+    } else {
+        inGame();
+    }
+}
+
+function numberIsValid(nstr, min=-Infinity, max=Infinity) {
+    if (!isNaN(nstr) && parseInt(Number(nstr)) == nstr && !isNaN(parseInt(nstr, 10)) ) {
+        if ( parseInt(Number(nstr))>min && parseInt(Number(nstr))<max) {
+            return true;
+        }
+    }
+    return false;
+}
+
+document.getElementById("play").addEventListener("click", turn);
+
+document.getElementById("forfeit").addEventListener("click", () => {
+    let msg = "Pourquoi vous abandonnez??\nVous avez perdu, le numero était " + hiddenNumber;
+    let mode = "failure";
+
+    // update message
+    print_msg(msg, mode);
+
+    // update buttons
+    toNewGame();
+}
+);
+
+document.getElementById("newgame").addEventListener("click", () => { print_msg("", "normal"); inGame(); });
+
