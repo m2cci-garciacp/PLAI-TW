e5cf89807bb624207f40656683e28ab8a9199664
m2cci-garciacp <pablo.garcia-campos@etu.univ-grenoble-alpes.fr>
2023-11-10 14:19:48 +0100

    fait nombreChache du TP7

--- TP7/ChampFleurs/nombrecache/js/game.js
+++ TP7/ChampFleurs/nombrecache/js/game.js
@@ -1,14 +1,16 @@
 let tryLefts = 10;
-const hiddenNumber=45;
-let newgame = true;
+let hiddenNumber=Math.floor(Math.random() * 101);
+let newgame = false;
 
 
-document.getElementById("play").onclick( () => {
+document.getElementById("play").addEventListener("click",  () => {
     let guess = document.getElementById("guess").value;
     let msg1 = "";
     let msg2 = "";
     let mode;
-    
+
+    tryLefts-=1;
+
     if (guess > hiddenNumber) {
         msg1 = "Trop grand!\n";
         msg2 = "Rejouez, il vous reste "+ tryLefts+ " essais.";
@@ -20,41 +22,59 @@ document.getElementById("play").onclick( () => {
     } else {
         msg1 = "Bravo!\nVous avez gagnez en "+ (10-tryLefts)+ " essais.";
         mode = "success";
-        newgame = false;
+        newgame = true;
     }
 
-    tryLefts-=1;
-
-    if (tryLefts === 0 && not (newgame)) {
+    if (tryLefts === 0 && !newgame ) {
         msg2 = "Vous avez perdu, le numero était " + hiddenNumber;
-        mode = "lost";
+        mode = "failure";
+        newgame = true;
     } 
 
     // update message
     print_msg(msg1+msg2, mode);
 
     // update buttons
-        
-
+    if (newgame) {
+        toNewGame();
+    } else {
+        inGame();
     }
-)
+})
 
-document.getElementById("forfeit").onclick( () => {
+document.getElementById("forfeit").addEventListener("click", () => {
     let msg = "Pourquoi vous abandonnez??\nVous avez perdu, le numero était " + hiddenNumber;
-    let mode = "lost";
+    let mode = "failure";
 
     // update message
     print_msg(msg, mode);
 
     // update buttons
-        
-
+    toNewGame();
     }
 )
+
+document.getElementById("newgame").addEventListener("click", () => {print_msg("", "normal");inGame();});
+
+function inGame() {
+    document.getElementById("play").classList.remove("hidden");
+    document.getElementById("forfeit").classList.remove("hidden");
+    document.getElementById("newgame").classList.add("hidden");
+}
+
+function toNewGame() {
+    document.getElementById("play").classList.add("hidden");
+    document.getElementById("forfeit").classList.add("hidden");
+    document.getElementById("newgame").classList.remove("hidden");
+    tryLefts = 10;
+    newgame = false;
+    hiddennumber = Math.floor(Math.random() * 101);
+}
 
 function print_msg(msg, mode) {
     let textBox = document.getElementById("textBox");
 
-    textBox.innerText(msg);
-    textBox.classList.addClass(mode);
+    textBox.innerText = msg;
+    textBox.className = "";
+    textBox.classList.add(mode);
 }
