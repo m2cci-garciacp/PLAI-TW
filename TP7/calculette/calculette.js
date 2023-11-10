40a959bf7c7a4fb5bcab4339d70affbde06f759d
garciacp <pablo.garcia-campos@etu.univ-grenoble-alpes.fr>
2023-11-10 23:16:12 +0100

    stylise la calculette. qq verifications faites, mais il faut reverifier tout

--- TP7/calculette/calculette.js
+++ TP7/calculette/calculette.js
@@ -1,67 +1,127 @@
 
+/**
+ * Global variables:
+ * operator (string) : last used operator
+ * ans (number) : last value calculated
+ * currentNumber (string) : current value input
+ * PRINT_LENGTH (number) : length maximal of printing screen
+ */
+let operator="+";
+let ans = 0;
+let currentNumber = "";
+const PRINT_LENGTH = 15;
 
-let operator;
-let ans=NaN;
-let currentNumber="";
-
-function pushCancel() {
-    ans=NaN;
-    n2=NaN;
-    showNumber("&nbsp;");
-}
-
-function pushNumber(e) {
-    currentNumber += e.target.innerText;
-    showNumber(currentNumber);
-}
-
-function pushOperator(e) {
-    getAns();
-    showNumber(ans.toString());
-    operator=e.target.innerHTML;
+/** 
+ * Called when Cancel button is pressed. It reinitialises everything.
+ * @param {string} msg message to print. Space by default.
+ */
+function pushCancel(e, msg = "&nbsp;") {
+    ans = 0;
     currentNumber = "";
+    operator = "+";
+    showNumber(msg);
 }
 
+/**
+ * Called when number button is pressed. This includes '0'-'9' and '.'.
+ * It adds the clicked symbol to the currentNumber and print its on screen. 
+ * @param {event} e is the event that calls the function.
+ */
+function pushNumber(e) {
+//     if (currentNumber.length < PRINT_LENGTH) {
+//         currentNumber += e.target.getAttribute("name");
+//     }
+if ( (currentNumber.substring(0, 1) === '-' && currentNumber.length <= PRINT_LENGTH)  || (currentNumber.substring(0, 1) !== '-' && currentNumber.length < PRINT_LENGTH) ) {
+    currentNumber += e.target.getAttribute("name");
+}
+showNumber(currentNumber);
+}
+
+/**
+ * Called when operator button is pressed. This includes '+', '-', '*' and '/'.
+ * It performs the calculation of previous operator and prints the result on screen. 
+ * @param {event} e is the event that calls the function.
+ */
+function pushOperator(e) {
+    if (currentNumber !== "" || operator === "=") {
+        getAns();
+        showNumber(ans.toString());
+        operator = e.target.getAttribute("name");
+        console.log(operator);
+        currentNumber = "";
+    } else {
+        ;
+    }
+}
+
+/**
+ * Prints the number on the screen, cut down to the PRINT_LENGTH size.
+ * @param {string} number number to be printed
+ */
 function showNumber(number) {
+    console.log("abc".indexOf("x"));
+    if (number === "&nbsp;" ) {
+        ;
+    } else if (number.indexOf('.') !== -1) {
+        if (Number(number) >= 0) {number = Number(number).toString().substring(0, PRINT_LENGTH );}
+        else {number = Number(number).toString().substring(0, PRINT_LENGTH + 1);}
+    } else if (number.length <= PRINT_LENGTH ) {
+        ;
+    } else if ( (String(number).substring(0, 1) === '-') && (number.length === (PRINT_LENGTH + 1)) ) {
+        number = number.substring(0, PRINT_LENGTH + 1);
+    } else {
+        number = "O.L";
+    }
     document.getElementById("screen").innerHTML = number;
 }
 
+/**
+ * Calculates the new value in fonction of last operator and stores in memory.
+ */
 function getAns() {
-    if (isNaN(ans)) {
-        ans = parseFloat(currentNumber);
-    } else if (operator==="+") {
+    if (operator === "+") {
         ans += parseFloat(currentNumber);
-    } else if (operator==="-") {
+    } else if (operator === "-") {
         ans -= parseFloat(currentNumber);
-    } else if (operator==="*") {
+    } else if (operator === "*") {
         ans *= parseFloat(currentNumber);
-    } else if (operator==="/") {
+    } else if (operator === "/") {
         ans /= parseFloat(currentNumber);
     }
 }
 
+/**
+ * Changes the sign of the current value.
+ */
 function pushChangeSign() {
-    ans *= -1;
-    showNumber(ans.toString());
+    if (currentNumber !== "") {
+        // cours d'edition
+        if (currentNumber.substring(0, 1) === "-") {
+            currentNumber = currentNumber.substring(1);
+        } else {
+            currentNumber = "-" + currentNumber;
+        }
+        showNumber(currentNumber);
+    } else {
+        // edition fini, ans present
+        ans *= -1;
+        showNumber(ans.toString());
+    }
+
+    // TODO: quand on a un ans sur ecran, chg de sign => -ans
+    //       mais          ans, chg de sign, op, chg de sign => ans, op
 }
 
-document.getElementById("0").addEventListener("click", pushNumber);
-document.getElementById("1").addEventListener("click", pushNumber);
-document.getElementById("2").addEventListener("click", pushNumber);
-document.getElementById("3").addEventListener("click", pushNumber);
-document.getElementById("4").addEventListener("click", pushNumber);
-document.getElementById("5").addEventListener("click", pushNumber);
-document.getElementById("6").addEventListener("click", pushNumber);
-document.getElementById("7").addEventListener("click", pushNumber);
-document.getElementById("8").addEventListener("click", pushNumber);
-document.getElementById("9").addEventListener("click", pushNumber);
-document.getElementById(".").addEventListener("click", pushNumber);
 
-document.getElementById("+").addEventListener("click", pushOperator);
-document.getElementById("-").addEventListener("click", pushOperator);
-document.getElementById("*").addEventListener("click", pushOperator);
-document.getElementById("/").addEventListener("click", pushOperator);
-document.getElementById("=").addEventListener("click", pushOperator);
+/*
+ * Call the event handlers for every key.
+ */
+for (const element of document.getElementsByClassName("number")) {
+    element.addEventListener("click", pushNumber);
+};
+for (const element of document.getElementsByClassName("operator")) {
+    element.addEventListener("click", pushOperator);
+};
 
 document.getElementById("C").addEventListener("click", pushCancel);
 
---
+++ TP7/calculette/checklist.md
@@ -0,0 +1,28 @@
+# Checklist:
+
+## Operations
+[x] numeros 0-9
+[x] Somme
+[x] Reste
+[x] Multiplication
+[x] Division
+[x] Egal
+[x] Operation+Operation
+[x] Egal+Operation
+[] Changement de signe avant operation
+[] Changement de signe apres operation
+[x] Point decimal
+[x] Operations point decimal
+[x] Decimal+decimal=entier
+[x] Cancel
+[x] Cancel+Cancel
+[x] Cancel+numero+operation+numero+op
+[x] Cancel+operation :
+
+
+## Format
+[x] nbre maximal de chiffres
+[x] avec minus et sans minus
+[x] decimal
+[x] decimal division
+[] error
